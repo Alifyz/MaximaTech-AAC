aafcbb00fe187133bdc631f0453d58ab3c5b6ea2
Alifyz <alifyz@outlook.com>
2019-05-03 12:06:44 -0300

    refactor: Criando a página de detalhes.

--- app/src/main/java/com/alifyz/newsapp/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/alifyz/newsapp/ui/details/DetailsFragment.kt
@@ -14,6 +14,7 @@ import kotlinx.android.synthetic.main.fragment_details.*
 class DetailsFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
         return inflater.inflate(R.layout.fragment_details, container, false)
     }
 
