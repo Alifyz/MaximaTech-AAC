5c36fd970c7bafa6dc1065df81e3e68d6b40c2d0
Alifyz <alifyz@outlook.com>
2019-05-03 12:14:16 -0300

    refactor: Criando a página de detalhes.

--- app/src/main/java/com/alifyz/newsapp/background/RefreshNews.kt
+++ app/src/main/java/com/alifyz/newsapp/background/RefreshNews.kt
@@ -3,15 +3,16 @@ package com.alifyz.newsapp.background
 import android.content.Context
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
-import com.alifyz.newsapp.repository.AppRepository
 import kotlinx.coroutines.delay
 
 class RefreshNews(context: Context, params : WorkerParameters) : CoroutineWorker(context, params) {
 
     override suspend fun doWork(): Result {
-        val repository = AppRepository(applicationContext)
-
         try {
+            //Operações que exigem muito da CPU
+            //OU tarefas async.
+            //1 - Realizar um Upload
+            //2 - Processar alguma Imagem etc
             delay(10000)
             return Result.success()
         } catch (e: Exception) {
